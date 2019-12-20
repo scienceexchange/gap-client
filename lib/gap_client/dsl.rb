b8c788e10bf4371d042c225fa862d1297df9e5a4
David Iorns <david@scienceexchange.com>
2019-12-20 13:47:07 +1300

    require

--- lib/gap_client/dsl.rb
+++ lib/gap_client/dsl.rb
@@ -6,13 +6,33 @@ module GapClient
 end
 
 require 'gap_client/dsl/attachments'
+require 'gap_client/dsl/cells'
+require 'gap_client/dsl/emails'
+require 'gap_client/dsl/forms'
+require 'gap_client/dsl/frequencies'
+require 'gap_client/dsl/frequency_units'
+require 'gap_client/dsl/jobs'
+require 'gap_client/dsl/lines'
+require 'gap_client/dsl/runs'
+require 'gap_client/dsl/tables'
 require 'gap_client/dsl/users'
+require 'gap_client/dsl/words'
 require 'gap_client/utils'
 
 module GapClient
   module DSL
     include Attachments
+    include Cells
+    include Emails
+    include Forms
+    include Frequencies
+    include FrequencyUnits
+    include Jobs
+    include Lines
+    include Runs
+    include Tables
     include Users
+    include Words
     include Utils
   end
 end
--- lib/gap_client/resources.rb
+++ lib/gap_client/resources.rb
@@ -7,4 +7,13 @@ end
 
 require 'gap_client/resources/object'
 require 'gap_client/resources/attachment'
+require 'gap_client/resources/cell'
+require 'gap_client/resources/email'
+require 'gap_client/resources/frequency'
+require 'gap_client/resources/frequency_unit'
+require 'gap_client/resources/job'
+require 'gap_client/resources/line'
+require 'gap_client/resources/run'
+require 'gap_client/resources/table'
 require 'gap_client/resources/user'
+require 'gap_client/resources/word'
