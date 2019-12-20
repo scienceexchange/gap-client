403b3c510571dfcfab0df66ab71a7c90c6f9649e
David Iorns <david@scienceexchange.com>
2019-12-20 13:20:54 +1300

    spec

--- spec/lib/gap_client/dsl/attachments_spec.rb
+++ spec/lib/gap_client/dsl/attachments_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe 'GapClient::DSL::Attachments' do
+  describe '#get_attachment' do
+    it 'gets an attachment' do
+      VCR.use_cassette('get_attachment') do
+        quote_response = GAP_CLIENT.get_attachment(id: 189)
+        expect(quote_response).to be_a(GapClient::Resources::Attachment)
+      end
+    end
+  end
+
   describe '#get_attachments' do
     it 'gets attachments' do
       VCR.use_cassette('get_attachments') do
